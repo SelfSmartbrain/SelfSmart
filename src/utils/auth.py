a5628a6104897f970f4d356fbdf818c3dc5ed986
genius-0963 <geniusthegreat0963@gmail.com>
2026-07-12 00:07:01 +0530

    Update src/utils/auth.py

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -41,7 +41,7 @@ def create_access_token(data: Dict[str, Any], expires_delta: Optional[timedelta]
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
-    
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
